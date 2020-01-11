3528d9e53dfe1f2ef31ddd2f2c150291529a5460
JasperDre <jasperdre@gmail.com>
2020-01-11 19:57:31 +0100

    Revert "Merge branch 'opengl-4'"
    This reverts commit 6272a5d7a19fda477dac170411afb42994dd58c3, reversing
    changes made to b46f8490f9cf1f6eddf25eff51d13559009c36e7.

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,99 @@
-#include "Engine.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void ErrorCallback(int arg_Error, const char* arg_Description)
+{
+	fprintf(stderr, "Error: %s\n", arg_Description);
+}
+
+void KeyCallback(GLFWwindow* arg_Window, int arg_Key, int arg_Scancode, int arg_Action, int arg_Mode)
+{
+	if (arg_Key == GLFW_KEY_ESCAPE && arg_Action == GLFW_PRESS)
+		glfwSetWindowShouldClose(arg_Window, GL_TRUE);
+}
 
 int main(int argc, char** argv)
 {
-	Engine engine;
-	engine.Setup();
-	engine.Loop();
-	engine.Destroy();
+	// Init GLFW
+	GLFWwindow* window;
+
+	glfwSetErrorCallback(ErrorCallback);
+
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(1240, 720, "Hello OpenGL", NULL, NULL);
+
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, KeyCallback);
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Init GLAD
+	if (!gladLoadGL())
+	{
+		printf("Something went wrong!\n");
+		exit(-1);
+	}
+
+	glfwSwapInterval(1);
+
+	// Print version numbers
+	int major, minor, revision;
+	glfwGetVersion(&major, &minor, &revision);
+	printf("GLFW %d.%d.%d\n", major, minor, revision);
+	printf("OpenGL %s\n", glGetString(GL_VERSION));
+	printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	double currentFrame = glfwGetTime();
+	double lastFrame = currentFrame;
+	double deltaTime;
+
+	// Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		int width, height;
+		float ratio;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+		
+		currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		glViewport(0, 0, width, height);
+		glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		printf("dt %f\n", deltaTime);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Destroy
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
