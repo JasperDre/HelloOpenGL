6179119b101abbab14d8d981f483e732b13ccbdd
JasperDre <jasperdre@gmail.com>
2020-07-23 21:58:35 +0200

    Call build script to generate files

--- Generate.bat
+++ Generate.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -Ax64 ..
+cmake -DGLFW_USE_HYBRID_HPG=ON -G "Visual Studio 16 2019" -Ax64 ..
 pause
--- Setup.bat
+++ Setup.bat
@@ -2,7 +2,5 @@ echo 'Initializing submodules and dependencies'
 git submodule init
 git submodule update
 echo 'Generating solution'
-mkdir build
-cd build
-cmake -G "Visual Studio 16 2019" -Ax64 ..
+call Generate.bat
 pause
