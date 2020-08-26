dd164d7db2f660c8d879f741d96b13cac3fe040f
JasperDre <jasperdre@gmail.com>
2020-08-26 23:51:41 +0200

    Suppress assimp developer warnings

--- Generate.bat
+++ Generate.bat
@@ -1,4 +1,4 @@
 mkdir Build
 cd Build
-cmake -DGLFW_USE_HYBRID_HPG=ON -DLIBRARY_SUFFIX="" -G "Visual Studio 16 2019" -Ax64 ..
+cmake -DGLFW_USE_HYBRID_HPG=ON -Wno-dev -DLIBRARY_SUFFIX="" -G "Visual Studio 16 2019" -Ax64 ..
 pause
